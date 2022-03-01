f5da20e0f9dc1d8717a668a762c319cae64180ab
DGJ <1476154535@qq.com>
2022-03-01 14:49:09 +0800

    rabbitmq

---
+++ TS-ModuleMQ/src/main/java/com/trojan/mq/MQApp.java
@@ -0,0 +1,11 @@
+package com.trojan.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MQApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MQApp.class, args);
+    }
+}
