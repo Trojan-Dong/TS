00cf286d7e10d6bd2d2b26e9397a66628b3d5d33
guoj.dong <1476154535@qq.com>
2023-08-08 13:37:59 +0800

    cherry pick

--- TS-ModuleTwo/src/test/java/git/GitTest.java
+++ TS-ModuleTwo/src/test/java/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
      */
     public static void main(String[] args) {
         System.out.println("git test");
+        
+        System.out.println("git test cherry pick");
     }
 }
